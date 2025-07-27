8a54d33730aae76a5c850a2d8492cbd8df4cf60b
Matthew McClure <108655204+MatMcClure@users.noreply.github.com>
2025-07-27 15:06:47 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import Home from './pages/Home'; // import the Home component
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
